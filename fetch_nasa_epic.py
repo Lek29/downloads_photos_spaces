f665f0ea32eb48dc6ea3b99f448e4563df894286
Lek29 <ligioner29@mail.ru>
2024-11-16 19:09:34 +0800

    разбил код по файлам

---
+++ fetch_nasa_epic.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+import requests
+
+
+def get_epic_images(api_key, count=1):
+    """
+       Получает ссылки на фотографии с EPIC API от NASA.
+
+       Args:
+           api_key (str): API-ключ для аутентификации запроса.
+           count (int, optional): Количество фотографий для получения. По умолчанию 1.
+
+       Returns:
+           list: Список URL для скачивания фотографий.
+
+       Raises:
+           requests.exceptions.HTTPError: Если запрос к API завершился неудачно.
+       """
+    url = f'https://api.nasa.gov/EPIC/api/natural/images'
+    params = {
+        'api_key': api_key
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    images_items = response.json()[:int(count)]
+    image_urls = []
+
+    for image_data in images_items:
+        image_name = image_data['image']
+        date_str = image_data['date']
+        date_obj = datetime.fromisoformat(date_str)
+        date_formatted = date_obj.strftime('%Y/%m/%d')
+
+        image_url = f'https://epic.gsfc.nasa.gov/archive/natural/{date_formatted}/png/{image_name}.png'
+        image_urls.append(image_url)
+
+    return image_urls
---
+++ fetch_spacex_images.py
@@ -0,0 +1,47 @@
+import os
+
+import requests
+
+
+def fetch_spacex_launch(launch_id=None):
+    '''
+     Получает ссылки на фотографии с последнего запуска SpaceX.
+
+    Yields:
+        tuple: Кортеж, содержащий URL изображения и путь для сохранения.
+
+    Raises:
+        requests.exceptions.HTTPError: Если запрос к API SpaceX завершился неудачно.
+        KeyError: Если данные о запуске не содержат ожидаемых ключей.
+
+    Returns:
+        None
+    '''
+    url_mask = 'https://api.spacexdata.com/v5/launches/'
+
+    if launch_id:
+        response = requests.get(f'{url_mask}{launch_id}')
+    else:
+        response = requests.get(url_mask)
+        response.raise_for_status()
+        last_launch = 0
+        launch_id = response.json()[last_launch]['id']
+        response = requests.get(f'{url_mask}{launch_id}')
+
+    response.raise_for_status()
+    launch_items = response.json()
+
+    if isinstance(launch_items, list):
+        for launch in launch_items:
+            if launch['id'] == launch_id:
+                launch_items = launch
+                break
+
+
+    links_photos = launch_items.get('links', {}).get('flickr', {}).get('original', [])
+    return links_photos
+
+    # for number_foto, link in enumerate(links_photos, start=1):
+    #     save_path = os.path.join(r'C:\pythonProject\pythonProject\Download_photo\images', '')
+    #
+    #     yield link, save_path
