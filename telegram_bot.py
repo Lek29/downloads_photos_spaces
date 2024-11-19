b0561bd62e269586af60346b4e3ce681b60b9040
Lek29 <ligioner29@mail.ru>
2024-11-20 06:25:41 +0800

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,13 +3,21 @@ import telegram
 from dotenv import load_dotenv
 
 
+def main():
+    load_dotenv()
 
-load_dotenv()
+    token_tg_bot = os.getenv('TOKEN_TG_BOT')
+    channel_id = os.getenv('CHANNEL_ID')
 
-token_tg_bot = os.getenv('TOKEN_TG_BOT')
-channel_id = os.getenv('CHANNEL_ID')
+    text = 'It`s my first message!'
 
-text = 'It`s my first message!'
+    bot = telegram.Bot(token=token_tg_bot)
+    
 
-bot = telegram.Bot(token=token_tg_bot)
-bot.send_message(text=text, chat_id=channel_id)
+    photo_path = 'nasa_apod_directory/apod_1.jpg'
+
+    with open(photo_path, 'rb') as photo:
+        bot.send_photo(chat_id=channel_id, photo=photo)
+
+if __name__ == '__main__':
+    main()
