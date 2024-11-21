a985282ff302ce6d55d60f5174efde79b0aba963
Lek29 <ligioner29@mail.ru>
2024-11-21 19:06:23 +0800

    создан скрипт который сам постит фото в тг

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,13 +11,13 @@ def main():
 
     text = 'It`s my first message!'
 
-    bot = telegram.Bot(token=token_tg_bot)
-    
+    bot = telegram.Bot(token=token_tg_bot) 
 
     photo_path = 'nasa_apod_directory/apod_1.jpg'
 
     with open(photo_path, 'rb') as photo:
         bot.send_photo(chat_id=channel_id, photo=photo)
+
 
 if __name__ == '__main__':
     main()
